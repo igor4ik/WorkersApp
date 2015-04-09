6279120e657bc0486efd6634de4de436691ea459
igor4ik <igor4ik83@gmail.com>
2015-04-09 22:13:33 +0300

    bug fixed
    add multibinding

---
+++ WorkerApp/ConverterForMultiBinding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WorkerApp
+{
+    class ConverterForMultiBinding : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return values[0];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Array.ConvertAll<Type, Object>(targetTypes, t => value);
+        }
+    }
+}
